b877c169f9b599f19407b3e69bd3c95eab33c8e4
mrtillman <askmrtillman@gmail.com>
2019-09-22 00:29:33 -0400

    configure mvc

--- Startup.cs
+++ Startup.cs
@@ -9,27 +9,25 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace aspnetcore_authorization_request
 {
-    public class Startup
+  public class Startup
+  {
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services)
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                var signInButton = @"<a href='#'>Sign In<a/>";
-                await context.Response.WriteAsync(signInButton);
-            });
-        }
+      services.AddMvc();
     }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseMvcWithDefaultRoute();
+
+    }
+  }
 }
