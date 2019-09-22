cc0a79e0c7aa6dd35a5762c45b45220fcdb45d93
mrtillman <askmrtillman@gmail.com>
2019-09-22 13:49:44 -0400

    add logic to get access token

--- Controllers/OAuth2.cs
+++ Controllers/OAuth2.cs
@@ -1,32 +1,37 @@
 using Microsoft.Extensions.Configuration;
 using System.Collections.Specialized;
 using System.Collections.Generic;
-
+using System.Threading.Tasks;
+using System.Net.Http;
+using AuthDemo.Constants;
 public class OAuth2Controller : BaseController
 {
 
     public OAuth2Controller(IConfiguration Configuration)
         :base(Configuration) { }
 
-    public string Callback()
+    public async Task<string> Callback()
     {
         var code = Request.Query["code"];
-        return code;
+        return await getToken(code);
     }
 
-    // private void getToken(string code) {
-    //     var keyValues = new List<KeyValuePair<string, string>>();
-    //     /*
-    //      code,
-    //     redirect_uri,
-    //     client_id,
-    //     client_secret,
-    //     scope: 'openid',
-    //     grant_type: 'authorization_code',
-    //     */
-    //     keyValues.Add(new KeyValuePair<string, string>("code", code));
-    //     var content = new FormUrlEncodedContent(keyValues);
-    //     client.PostAsync("requestUri", content);
-    // }
+    private async Task<string> getToken(string code) {
+        var keyValues = new List<KeyValuePair<string, string>>();
+        var baseUrl = ServerUrls.SECURE[ENV.DEV];
+        var requestUri = $"{baseUrl}/connect/token";
+        var redirect_uri = configuration["REDIRECT_URI"].ToString();
+        var client_id = configuration["CLIENT_ID"].ToString();
+        var client_secret = configuration["CLIENT_SECRET"].ToString();
+        keyValues.Add(new KeyValuePair<string, string>("code", code));
+        keyValues.Add(new KeyValuePair<string, string>("redirect_uri", redirect_uri));
+        keyValues.Add(new KeyValuePair<string, string>("client_id", client_id));
+        keyValues.Add(new KeyValuePair<string, string>("client_secret", client_secret));
+        keyValues.Add(new KeyValuePair<string, string>("scope", "openid"));
+        keyValues.Add(new KeyValuePair<string, string>("grant_type", "authorization_code"));
+        var content = new FormUrlEncodedContent(keyValues);
+        var response = await client.PostAsync(requestUri, content);
+        return await response.Content.ReadAsStringAsync();
+    }
 
 }
