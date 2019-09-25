fda268fbc7fb9bfc5901972c2d580c06fca17d5c
mrtillman <askmrtillman@gmail.com>
2019-09-25 19:02:09 -0400

    fix formatting

--- Models/AuthResponse.cs
+++ Models/AuthResponse.cs
@@ -4,7 +4,7 @@ using System.Net.Http;
 
 public class AuthResponse
 {
-     public string access_token { get; set; }
+    public string access_token { get; set; }
     public string expires_in { get; set; }
     public string token_type { get; set; }
     public string scope { get; set; }
