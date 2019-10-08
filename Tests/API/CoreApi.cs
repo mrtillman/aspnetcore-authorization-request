46319a18691e68656ed8b3ea11b9d1827768a128
mrtillman <askmrtillman@gmail.com>
2019-10-07 23:28:17 -0400

    simplify tests using mocks

--- Tests/API/CoreApi.cs
+++ Tests/API/CoreApi.cs
@@ -1,12 +1,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Microsoft.Extensions.Configuration;
 using AuthDemo.API;
-using AuthDemo.Tests.Mocks;
+using AuthDemo.TestDoubles;
 using System.Net;
 using System.Net.Http;
-using Moq;
-using Moq.Protected;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace AuthDemo.Tests
@@ -17,29 +13,23 @@ namespace AuthDemo.Tests
     private CoreApi coreApi { get; set; }
 
     [TestMethod]
-    public async Task Should_Work()
+    public async Task Should_Get_Counters()
     {
+      var mockResponse = Mock.SetUp(response =>
+      {
+        response.StatusCode = HttpStatusCode.OK;
+        response.Content = new StringContent(Stub.JSON.Counters);
+        return response;
+      });
 
-      var mockResponse = Mock.Of<HttpResponseMessage>();
-      mockResponse.StatusCode = HttpStatusCode.OK;
+      var mockHandler = Mock.HttpResponseMessage(mockResponse);
 
-      var jsonCounters = "[{\"_id\":\"5d16c0cd11ee4a3d6f44b045\",\"name\":\"alcohol\",\"value\":0,\"skip\":1,\"__v\":0},{\"_id\":\"5d16c0cd11ee4a3d6f44b046\",\"name\":\"tobacco\",\"value\":0,\"skip\":1,\"__v\":0},{\"_id\":\"5d16c0cd11ee4a3d6f44b047\",\"name\":\"firearms\",\"value\":0,\"skip\":1,\"__v\":0}]";
-      mockResponse.Content = new StringContent(jsonCounters);
+      var httpClient = new HttpClient(mockHandler.Object);
 
-      var mockMessageHandler = new Mock<HttpMessageHandler>();
-      mockMessageHandler.Protected()
-                        .Setup<Task<HttpResponseMessage>>(
-                            "SendAsync",
-                            ItExpr.IsAny<HttpRequestMessage>(),
-                            ItExpr.IsAny<CancellationToken>()
-                        )
-                        .ReturnsAsync(mockResponse);
-
-      var httpClient = new HttpClient(mockMessageHandler.Object);
-
-      coreApi = new CoreApi(MockObjects.Configuration, MockObjects.ServerUrls, httpClient);
+      coreApi = new CoreApi(Mock.Configuration, Mock.ServerUrls, httpClient);
 
       var result = await coreApi.GetCounters();
+
       Assert.IsTrue(result.DidSucceed);
     }
   }
