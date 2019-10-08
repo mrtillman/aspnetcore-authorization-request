271cc88112ffcc0bef4ccc03c6475bc8e89a29ec
mrtillman <askmrtillman@gmail.com>
2019-10-07 22:02:50 -0400

    isolate mocks; fix breaking changes; simplify tests

--- Tests/API/CoreApi.cs
+++ Tests/API/CoreApi.cs
@@ -1,58 +1,46 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.Extensions.Configuration;
 using AuthDemo.API;
-using AuthDemo.Constants;
+using AuthDemo.Tests.Mocks;
 using System.Net;
 using System.Net.Http;
 using Moq;
 using Moq.Protected;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Web;
 
 namespace AuthDemo.Tests
 {
   [TestClass]
-    public class CoreApiTests
+  public class CoreApiTests
+  {
+    private CoreApi coreApi { get; set; }
+
+    [TestMethod]
+    public async Task Should_Work()
     {
-        public CoreApiTests(){
 
-            // TODO: refactor
+      var mockResponse = Mock.Of<HttpResponseMessage>();
+      mockResponse.StatusCode = HttpStatusCode.OK;
 
-            var mockConfiguration = Mock.Of<IConfiguration>();
+      var jsonCounters = "[{\"_id\":\"5d16c0cd11ee4a3d6f44b045\",\"name\":\"alcohol\",\"value\":0,\"skip\":1,\"__v\":0},{\"_id\":\"5d16c0cd11ee4a3d6f44b046\",\"name\":\"tobacco\",\"value\":0,\"skip\":1,\"__v\":0},{\"_id\":\"5d16c0cd11ee4a3d6f44b047\",\"name\":\"firearms\",\"value\":0,\"skip\":1,\"__v\":0}]";
+      mockResponse.Content = new StringContent(jsonCounters);
 
-            var mockServerUrls = Mock.Of<IServerUrls>();
-            Mock.Get(mockServerUrls)
-                .Setup(urls => urls.API)
-                .Returns("https://api.counter-culture.io");
+      var mockMessageHandler = new Mock<HttpMessageHandler>();
+      mockMessageHandler.Protected()
+                        .Setup<Task<HttpResponseMessage>>(
+                            "SendAsync",
+                            ItExpr.IsAny<HttpRequestMessage>(),
+                            ItExpr.IsAny<CancellationToken>()
+                        )
+                        .ReturnsAsync(mockResponse);
 
-            var mockResponse = Mock.Of<HttpResponseMessage>();
-            mockResponse.StatusCode = HttpStatusCode.OK;
-            
-            var jsonCounters = "[{\"_id\":\"5d16c0cd11ee4a3d6f44b045\",\"name\":\"alcohol\",\"value\":0,\"skip\":1,\"__v\":0},{\"_id\":\"5d16c0cd11ee4a3d6f44b046\",\"name\":\"tobacco\",\"value\":0,\"skip\":1,\"__v\":0},{\"_id\":\"5d16c0cd11ee4a3d6f44b047\",\"name\":\"firearms\",\"value\":0,\"skip\":1,\"__v\":0}]";
-            mockResponse.Content = new StringContent(jsonCounters);
+      var httpClient = new HttpClient(mockMessageHandler.Object);
 
-            var mockMessageHandler = new Mock<HttpMessageHandler>();
-            mockMessageHandler.Protected()
-                              .Setup<Task<HttpResponseMessage>>(
-                                  "SendAsync",
-                                  ItExpr.IsAny<HttpRequestMessage>(),
-                                  ItExpr.IsAny<CancellationToken>()
-                              )
-                              .ReturnsAsync(mockResponse);
+      coreApi = new CoreApi(MockObjects.Configuration, MockObjects.ServerUrls, httpClient);
 
-            var httpClient = new HttpClient(mockMessageHandler.Object);
-
-            coreApi = new CoreApi(mockConfiguration, mockServerUrls, httpClient);
-        }
-
-        private CoreApi coreApi { get; set; }
-
-        [TestMethod]
-        public async Task Should_Work()
-        {
-            var result = await coreApi.GetCounters();
-            Assert.IsTrue(result.DidSucceed);
-        }
+      var result = await coreApi.GetCounters();
+      Assert.IsTrue(result.DidSucceed);
     }
+  }
 }
