9663d4a556ceca27f7d0a6d00ea7d5aa44eae92d
mrtillman <askmrtillman@gmail.com>
2019-10-08 22:21:21 -0400

    mock client id + redirect uri

--- Tests/TestDoubles/Mocks/Configuration.cs
+++ Tests/TestDoubles/Mocks/Configuration.cs
@@ -6,7 +6,16 @@ namespace AuthDemo.TestDoubles
   {
     public static IConfiguration Configuration
     {
-      get => Moq.Mock.Of<IConfiguration>();
+      get {
+        var configuration = Moq.Mock.Of<IConfiguration>();
+        Moq.Mock.Get(configuration)
+                .Setup(config => config["CLIENT_ID"])
+                .Returns("CLIENT_ID");
+        Moq.Mock.Get(configuration)
+                .Setup(config => config["REDIRECT_URI"])
+                .Returns("REDIRECT_URI");
+        return configuration;
+      }
     }
   }
 }
