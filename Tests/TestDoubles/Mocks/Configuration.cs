7c48bbcd60ae518f464e2711a9128bac78dd724f
mrtillman <askmrtillman@gmail.com>
2019-10-08 22:39:01 -0400

    mock client secret

--- Tests/TestDoubles/Mocks/Configuration.cs
+++ Tests/TestDoubles/Mocks/Configuration.cs
@@ -12,6 +12,9 @@ namespace AuthDemo.TestDoubles
                 .Setup(config => config["CLIENT_ID"])
                 .Returns("CLIENT_ID");
         Moq.Mock.Get(configuration)
+                .Setup(config => config["CLIENT_SECRET"])
+                .Returns("CLIENT_SECRET");
+        Moq.Mock.Get(configuration)
                 .Setup(config => config["REDIRECT_URI"])
                 .Returns("REDIRECT_URI");
         return configuration;
