14bceb120d7fdbbc7494da8c7991d0b9a95b1382
mrtillman <askmrtillman@gmail.com>
2019-11-10 17:57:18 -0500

    use strict mock behavior + setup mock api token value

--- Tests/Application/GetCounters.cs
+++ Tests/Application/GetCounters.cs
@@ -16,12 +16,16 @@ namespace Tests.Application
 
     [TestMethod]
     public async Task Should_Get_Counters() {
-      var mockCoreApi = Mock.Of<ICoreApi>();
+      var token = "TokenValue";
+      var mockCoreApi = Mock.Of<ICoreApi>(Moq.MockBehavior.Strict);
       Result<List<Counter>> mockResult = Result<List<Counter>>.Ok(new List<Counter>());
+      Mock.Get(mockCoreApi).SetupSet(api => api.Token = token).Verifiable();
+      Mock.Get(mockCoreApi).SetupGet(api => api.Token).Returns(token);
       Mock.Get(mockCoreApi)
           .Setup(api => api.GetCounters())
           .Returns(Task.FromResult(mockResult));
       getCountersUseCase = new GetCountersUseCase(mockCoreApi);
+      getCountersUseCase.Token = token;
       var result = await getCountersUseCase.Execute();
       Assert.IsTrue(result.DidSucceed);
     }
