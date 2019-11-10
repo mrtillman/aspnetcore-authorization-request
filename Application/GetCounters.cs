0ab39100f44226b1886c3ccd19198ada64cb3b4a
mrtillman <askmrtillman@gmail.com>
2019-11-10 14:21:18 -0500

    add use cases

---
+++ Application/GetCounters.cs
@@ -0,0 +1,31 @@
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Common;
+using Services;
+using Domain;
+
+namespace Application
+{
+  public class GetCountersUseCase : IUseCase<Task<Result<List<Counter>>>>
+  {
+    private ICoreApi coreApi { get; set; }
+    public string Token { get; set; }
+    public GetCountersUseCase(ICoreApi CoreApi)
+    {
+        coreApi = CoreApi;
+    }
+    public async Task<Result<List<Counter>>> Execute()
+    {
+      coreApi.Token = Token;
+
+      Result<List<Counter>> countersResult = await coreApi.GetCounters();
+
+      if (countersResult.DidFail)
+      {
+        return Result<List<Counter>>.Fail(countersResult.ErrorMessage);
+      }
+
+      return Result<List<Counter>>.Ok(countersResult.Value);
+    }
+  }
+}
