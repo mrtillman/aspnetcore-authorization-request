dd8278825052f8c5233246ad96dbf2758894aaba
mrtillman <askmrtillman@gmail.com>
2020-02-22 22:28:44 -0500

    remove redundant code

--- Application/GetTokenUseCase.cs
+++ Application/GetTokenUseCase.cs
@@ -5,7 +5,7 @@ using Domain;
 
 namespace Application
 {
-  public class GetTokenUseCase : IUseCase<Task<Result<string>>>
+  public class GetTokenUseCase : IUseCase<Task<Result<AuthorizationResponse>>>
   {
     public GetTokenUseCase(ISecureService SecureService)
     {
@@ -14,16 +14,9 @@ namespace Application
     private ISecureService secureService { get; set; }
     public string Code { get; set; }
     public string State { get; set; }
-    public async Task<Result<string>> Execute()
+    public async Task<Result<AuthorizationResponse>> Execute()
     {
-      Result<AuthorizationResponse> authResult = await secureService.GetToken(Code, State);
-
-      if (authResult.DidFail)
-      {
-        return Result<string>.Fail(authResult.ErrorMessage);
-      }
-
-      return Result<string>.Ok(authResult.Value.access_token);
+      return await secureService.GetToken(Code, State);
     }
   }
 }
