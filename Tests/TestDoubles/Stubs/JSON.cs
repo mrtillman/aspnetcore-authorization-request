3f228e620a37a1584d5f56fbccbcb51f026d1b2b
mrtillman <askmrtillman@gmail.com>
2020-02-22 22:38:08 -0500

    minor edit

--- Tests/TestDoubles/Stubs/JSON.cs
+++ Tests/TestDoubles/Stubs/JSON.cs
@@ -7,7 +7,7 @@ namespace Tests.TestDoubles
         => "[{\"_id\":\"5d16c0cd11ee4a3d6f44b045\",\"name\":\"alcohol\",\"value\":0,\"skip\":1,\"__v\":0},{\"_id\":\"5d16c0cd11ee4a3d6f44b046\",\"name\":\"tobacco\",\"value\":0,\"skip\":1,\"__v\":0},{\"_id\":\"5d16c0cd11ee4a3d6f44b047\",\"name\":\"firearms\",\"value\":0,\"skip\":1,\"__v\":0}]";
 
       public static string AuthorizationResponse
-        => "{\"id_token\":\"id_token\",   \"access_token\":\"access_token\", \"expires_in\":86400, \"token_type\":\"Bearer\",  \"scope\":\"openid\"}";
+        => "{\"id_token\":\"id_token\", \"access_token\":\"access_token\", \"expires_in\":86400, \"token_type\":\"Bearer\",  \"scope\":\"openid\"}";
     }
   }
 }
