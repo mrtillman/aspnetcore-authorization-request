b9f0577554d8b31ca736c50b4b2e02b4f6c68f4a
mrtillman <askmrtillman@gmail.com>
2020-03-02 02:13:08 -0500

    cache access token

--- Presentation/Controllers/OAuth2.cs
+++ Presentation/Controllers/OAuth2.cs
@@ -2,6 +2,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Application;
 using Services;
+using Common;
+using Domain;
 
 public class OAuth2Controller : Controller
 {
@@ -9,35 +11,45 @@ public class OAuth2Controller : Controller
   public OAuth2Controller(
     GetTokenUseCase GetTokenUseCase, 
     GetCountersUseCase GetCountersUseCase,
-    RenewTokenUseCase RenewTokenUseCase)
+    RenewTokenUseCase RenewTokenUseCase,
+    ICacheService CacheService)
   {
     getTokenUseCase = GetTokenUseCase;
     getCountersUseCase = GetCountersUseCase;
     renewTokenUseCase = RenewTokenUseCase;
+    cache = CacheService;
   }
   private GetTokenUseCase getTokenUseCase { get; set; }
   private GetCountersUseCase getCountersUseCase { get; set; }
   private RenewTokenUseCase renewTokenUseCase { get; set; }
 
+  private ICacheService cache { get; set; }
+
   // 2. Authorization Grant (inbound)
   public async Task<ActionResult> Callback(string code, string state)
   {
 
-    // 3. Authorization Grant (outbound)
-    
-    getTokenUseCase.Code = code;
-    getTokenUseCase.State = state;
+    var authResponse = cache.GetValue<AuthorizationResponse>(KEYS.ACCESS_TOKEN);
 
-    var tokenResult = await getTokenUseCase.Execute();
-    
-    if (tokenResult.DidFail)
-    {
-      return Unauthorized(tokenResult.ErrorMessage);
-    }
-    
-    var authResponse  = tokenResult.Value;
-    
-    renewTokenUseCase.RefreshToken = authResponse.refresh_token;
+    if(authResponse == null){
+
+      // 3. Authorization Grant (outbound)
+      getTokenUseCase.Code = code;
+      getTokenUseCase.State = state;
+
+      var tokenResult = await getTokenUseCase.Execute();
+      
+      if (tokenResult.DidFail)
+      {
+        return Unauthorized(tokenResult.ErrorMessage);
+      }
+      
+      authResponse = tokenResult.Value;
+
+      cache.SetValue(KEYS.ACCESS_TOKEN, authResponse);
+
+      renewTokenUseCase.RefreshToken = authResponse.refresh_token;
+    }    
 
     // 4. Access Token (inbound)
     getCountersUseCase.Token = authResponse.access_token;
