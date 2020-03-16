59d615ffbbb2e2a59c082049af125c0a73340d90
mrtillman <askmrtillman@gmail.com>
2020-03-15 22:05:01 -0400

    improve test coverage

--- Tests/Presentation/OAuth2ControllerTests.cs
+++ Tests/Presentation/OAuth2ControllerTests.cs
@@ -1,10 +1,43 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Presentation;
+using Application;
+using Moq;
+using Domain;
+using Common;
+using System.Threading.Tasks;
+using Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Tests.Presentation {
   
   [TestClass]
   public class OAuth2ControllerTests
   {
+    
+    public OAuth2Controller controller { get; set; }
 
+    public RenewTokenUseCase renewTokenUseCase { get; set; }
+    private Mock<ICacheService> cacheServiceMock { get; set; }
+
+    [TestInitialize]
+    public void TestSetup(){
+      var result = Result<AuthorizationResponse>.Ok(new AuthorizationResponse());
+    }
+
+    [TestMethod]
+    public async Task RenewTokenShould_RedirectHome_WhenRefreshTokenIsNull(){
+      cacheServiceMock = new Mock<ICacheService>();
+      cacheServiceMock.Setup(cache => cache.GetValue<string>(KEYS.REFRESH_TOKEN))
+                      .Returns(() => null);
+      renewTokenUseCase = new RenewTokenUseCase(null, cacheServiceMock.Object);
+      renewTokenUseCase.RefreshToken = null;
+      controller = new OAuth2Controller(
+        null, null, renewTokenUseCase, null
+      );
+      
+      var result = await controller.RenewToken() as RedirectResult;
+      
+      Assert.IsNotNull("/", result.Url);
+    }
   }
 }
