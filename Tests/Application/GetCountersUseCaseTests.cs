1fbe42b52bbbf7414a0bbad1711eddc1b8d57500
mrtillman <askmrtillman@gmail.com>
2020-03-20 20:15:27 -0400

    improve test coverage; tweak doubles

--- Tests/Application/GetCountersUseCaseTests.cs
+++ Tests/Application/GetCountersUseCaseTests.cs
@@ -12,22 +12,36 @@ namespace Tests.Application
   [TestClass]
   public class GetCountersUseCaseTests
   {
-    public GetCountersUseCase getCountersUseCase { get; set; }
+    public GetCountersUseCaseTests()
+    {
+        mockCounterService = new Mock<ICounterService>(MockBehavior.Strict);
+        Result<List<Counter>> mockResult = Result<List<Counter>>.Ok(TestDoubles.Counters);
+        mockCounterService.SetupSet(service => service.Token = TestDoubles.Token).Verifiable();
+        mockCounterService.SetupGet(service => service.Token)
+                          .Returns(TestDoubles.Token)
+                          .Verifiable();
+        mockCounterService.Setup(service => service.GetCounters())
+                          .Returns(Task.FromResult(mockResult));
+    }
+    private Mock<ICounterService> mockCounterService { get; set; }
+    private GetCountersUseCase getCountersUseCase { get; set; }
 
     [TestMethod]
-    public async Task Should_GetCounters() {
-      var token = "TokenValue";
-      var mockCountersService = Mock.Of<ICounterService>(MockBehavior.Strict);
-      Result<List<Counter>> mockResult = Result<List<Counter>>.Ok(new List<Counter>());
-      Mock.Get(mockCountersService).SetupSet(service => service.Token = token).Verifiable();
-      Mock.Get(mockCountersService).SetupGet(service => service.Token).Returns(token);
-      Mock.Get(mockCountersService)
-          .Setup(service => service.GetCounters())
-          .Returns(Task.FromResult(mockResult));
-      getCountersUseCase = new GetCountersUseCase(mockCountersService);
-      getCountersUseCase.Token = token;
-      var result = await getCountersUseCase.Execute();
-      Assert.IsTrue(result.DidSucceed);
+    public async Task Execute_Should_GetCounters() {
+      getCountersUseCase = new GetCountersUseCase(mockCounterService.Object);
+      getCountersUseCase.Token = TestDoubles.Token;
+
+      await getCountersUseCase.Execute();
+      
+      mockCounterService.Verify(service => service.GetCounters(), Times.Once);
+    }
+
+    [TestMethod]
+    public void Token_Should_GetToken() {
+      getCountersUseCase = new GetCountersUseCase(mockCounterService.Object);
+      var token = getCountersUseCase.Token;
+      
+      mockCounterService.Verify(service => service.Token, Times.Once);
     }
   }
 }
--- Tests/TestDoubles.cs
+++ Tests/TestDoubles.cs
@@ -1,10 +1,18 @@
 using Moq;
 using Common;
+using Domain;
+using System.Collections.Generic;
 using Microsoft.Extensions.Configuration;
 
 namespace Tests {
   public static class TestDoubles {
 
+    public static readonly string Token = "a2a3fa68-658a-4916-94ad-f1170a649839";
+    public static readonly string ClientId = "10ae9344-e599-4c49-ab0c-2891521ccc46";
+    public static readonly string ClientSecret = "013e6077-5ef1-4679-b353-a5792f9faabf";
+    public static readonly string RefreshToken = "8681bade-6c3a-4b86-a38a-378da2bf1181";
+    public static readonly string Code = "3bd5b7a0-876b-4a06-9aa8-903fe4d7e8e6";
+    public static readonly string State = "54719797-8ee7-488c-824a-f40fd9fab348";
     private static IServerUrls _serverUrls = null;
     public static IServerUrls ServerUrls { 
       get {
@@ -22,21 +30,26 @@ namespace Tests {
       }
     }
 
+    private static IConfiguration _configuration = null;
     public static IConfiguration Configuration
     {
       get {
-        var configuration = Mock.Of<IConfiguration>(MockBehavior.Strict);
-        Mock.Get(configuration)
-                .Setup(config => config["CLIENT_ID"])
-                .Returns("CLIENT_ID");
-        Mock.Get(configuration)
-                .Setup(config => config["CLIENT_SECRET"])
-                .Returns("CLIENT_SECRET");
-        Mock.Get(configuration)
-                .Setup(config => config["REDIRECT_URI"])
-                .Returns("REDIRECT_URI");
-        return configuration;
+        if(_configuration == null){
+          _configuration = Mock.Of<IConfiguration>(MockBehavior.Strict);
+          Mock.Get(_configuration)
+                  .Setup(config => config["CLIENT_ID"])
+                  .Returns("CLIENT_ID");
+          Mock.Get(_configuration)
+                  .Setup(config => config["CLIENT_SECRET"])
+                  .Returns("CLIENT_SECRET");
+          Mock.Get(_configuration)
+                  .Setup(config => config["REDIRECT_URI"])
+                  .Returns("REDIRECT_URI");
+          return _configuration;
+        }
+        return _configuration;
       }
     }
+    public static readonly List<Counter> Counters = new List<Counter>();
   }
 }
