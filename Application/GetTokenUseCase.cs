2f2122894159e3d6b7af675c80f86f35156ca6d1
mrtillman <askmrtillman@gmail.com>
2020-03-20 21:28:46 -0400

    return cached auth response if not null

--- Application/GetTokenUseCase.cs
+++ Application/GetTokenUseCase.cs
@@ -7,15 +7,19 @@ namespace Application
 {
   public class GetTokenUseCase : IUseCase<Task<Result<AuthorizationResponse>>>
   {
-    public GetTokenUseCase(ISecureService SecureService)
+    public GetTokenUseCase(ISecureService SecureService, ICacheService CacheService)
     {
         secureService = SecureService;
+        cacheService = CacheService;
     }
     private ISecureService secureService { get; set; }
+    private ICacheService cacheService { get; set; }
     public string Code { get; set; }
     public string State { get; set; }
     public async Task<Result<AuthorizationResponse>> Execute()
     {
+      var authResponse = cacheService.GetValue<AuthorizationResponse>(KEYS.ACCESS_TOKEN);
+      if(authResponse != null) return Result<AuthorizationResponse>.Ok(authResponse);
       return await secureService.GetToken(Code, State);
     }
   }
