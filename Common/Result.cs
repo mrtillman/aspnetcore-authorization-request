5ad5e90755b2ddb5b3ef02de9032938d916b67bc
mrtillman <askmrtillman@gmail.com>
2020-03-21 02:57:05 -0400

    incorporate static analog to simplify usage of fail and ok methods

--- Common/Result.cs
+++ Common/Result.cs
@@ -1,6 +1,5 @@
 namespace Common{
   public class Result<T> {
-
     public Result(bool DidSucceed, string ErrorMessage = "") {
       this.DidSucceed = DidSucceed;
       this.DidFail = !DidSucceed;
@@ -9,17 +8,18 @@ namespace Common{
     public bool DidSucceed { get; private set; }
     public bool DidFail { get; private set; }
     public string ErrorMessage { get; private set; }
-    public T Value { get; private set; }
+    public T Value { get; internal set; }
+  }
 
-    public static Result<T> Fail(string ErrorMessage){
+  public static class Result {
+    public static Result<T> Fail<T>(string ErrorMessage){
       return new Result<T>(false, ErrorMessage);
     }
 
-    public static Result<T> Ok(T Value){
+    public static Result<T> Ok<T>(T Value){
       var result = new Result<T>(true);
       result.Value = Value;
       return result;
     }
-
   }
 }
