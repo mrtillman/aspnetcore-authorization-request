73b6c60ac2fa398af22660d57462205728af60fc
mrtillman <askmrtillman@gmail.com>
2020-03-21 21:34:29 -0400

    improve test coverage

--- Tests/Common/ResultTests.cs
+++ Tests/Common/ResultTests.cs
@@ -9,8 +9,31 @@ namespace Tests.Common{
     public Result<bool> result { get; set; }
 
     [TestMethod]
-    public void Should_Work(){
-      Assert.IsTrue(true);
+    public void Ok_Should_Succeed(){
+      var result = Result.Ok(true);
+
+      Assert.IsTrue(result.DidSucceed);
+    }
+
+    [TestMethod]
+    public void Ok_Should_HaveValue(){
+      var result = Result.Ok(true);
+
+      Assert.IsTrue(result.Value);
+    }
+
+    [TestMethod]
+    public void Fail_Should_Fail(){
+      var result = Result.Fail<bool>("no luck");
+      
+      Assert.IsTrue(result.DidFail);
+    }
+
+    [TestMethod]
+    public void Fail_Should_HaveErrorMessage(){
+      var result = Result.Fail<bool>("no luck");
+      
+      Assert.IsNotNull(result.ErrorMessage);
     }
   }
 }
