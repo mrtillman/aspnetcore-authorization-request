ee63c5425487ac0fc2ff9811050f836ba7dfc384
mrtillman <askmrtillman@gmail.com>
2020-03-23 18:43:21 -0400

    improve test coverage

--- Tests/Common/AuthUrlRegExTests.cs
+++ Tests/Common/AuthUrlRegExTests.cs
@@ -6,11 +6,18 @@ namespace Tests.Common {
   [TestClass]
   public class AuthorizationUrlRegexTests {
 
-    public AuthorizationUrlRegex regex { get; set; }
+    private AuthorizationUrlRegex regex = new AuthorizationUrlRegex();
 
     [TestMethod]
-    public void Should_Work(){
-      Assert.IsTrue(true);
+    public void IsMatch_WhenAuthorizationUrlInvalid_ShouldFail(){
+      var url = "";
+      Assert.IsFalse(regex.IsMatch(url));
+    }
+
+    [TestMethod]
+    public void IsMatch_WhenAuthorizationUrlValid_ShouldSucceed(){
+      var url = "http://localhost:5000/connect/authorize?response_type=code&client_id={client-id}&redirect_uri={redirect_uri}&scope=openid offline_access&state={state}";
+      Assert.IsTrue(regex.IsMatch(url));
     }
   }
 }
